45ca362a2d015904eecad75dcb7a2a7475825076
Alan Cruz <alanlucascruz@gmail.com>
2023-01-17 14:43:56 -0300

    Removido opções de editar perfil e alterar senha

--- src/components/Header/HeaderDropdown.jsx
+++ src/components/Header/HeaderDropdown.jsx
@@ -1,18 +1,18 @@
-import { Fragment, memo, useEffect, useState } from "react";
+import { Fragment, memo, useEffect } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { signOut } from "../../store/slices/authSlice";
 import { useDispatch, useSelector } from "react-redux";
 
 import Dropdown from "../Dropdown";
-import ProfileModal from "./ProfileModal";
-import PasswordModal from "./PasswordModal";
+// import ProfileModal from "./ProfileModal";
+// import PasswordModal from "./PasswordModal";
 
 function HeaderDropdown() {
-  const [showProfileModal, setShowProfileModal] = useState(false);
-  const [showPasswordModal, setShowPasswordModal] = useState(false);
+  // const [showProfileModal, setShowProfileModal] = useState(false);
+  // const [showPasswordModal, setShowPasswordModal] = useState(false);
 
-  const toggleProfileModal = () => setShowProfileModal(!showProfileModal);
-  const togglePasswordModal = () => setShowPasswordModal(!showPasswordModal);
+  // const toggleProfileModal = () => setShowProfileModal(!showProfileModal);
+  // const togglePasswordModal = () => setShowPasswordModal(!showPasswordModal);
 
   const navigate = useNavigate();
   const dispatch = useDispatch();
@@ -35,12 +35,12 @@ function HeaderDropdown() {
         }
         content={
           <Fragment>
-            <Link onClick={toggleProfileModal}>
+            {/* <Link onClick={toggleProfileModal}>
               <i className="bi bi-person"></i> Editar Perfil
             </Link>
             <Link onClick={togglePasswordModal}>
               <i className="bi bi-lock"></i> Alterar Senha
-            </Link>
+            </Link> */}
             <Link onClick={() => dispatch(signOut())}>
               <i className="bi bi-box-arrow-right"></i> Sair
             </Link>
@@ -48,8 +48,8 @@ function HeaderDropdown() {
         }
       />
 
-      <ProfileModal show={showProfileModal} toggle={toggleProfileModal} />
-      <PasswordModal show={showPasswordModal} toggle={togglePasswordModal} />
+      {/* <ProfileModal show={showProfileModal} toggle={toggleProfileModal} />
+      <PasswordModal show={showPasswordModal} toggle={togglePasswordModal} /> */}
     </Fragment>
   );
 }
